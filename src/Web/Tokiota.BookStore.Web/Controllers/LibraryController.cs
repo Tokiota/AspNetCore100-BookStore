dcaec2611e822a4bce31210acad506999f1e10de
ericuss <etorreg@gmail.com>
2017-02-16 10:57:07 +0100

    Mvc controller

---
+++ src/Web/Tokiota.BookStore.Web/Controllers/LibraryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Tokiota.BookStore.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    public class LibraryController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
