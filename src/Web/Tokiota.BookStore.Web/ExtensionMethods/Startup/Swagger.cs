13b23d6fe330c870c7941400f7cd0657dc94d259
ericuss <etorreg@gmail.com>
2017-02-16 13:00:37 +0100

    add swagger documentation

---
+++ src/Web/Tokiota.BookStore.Web/ExtensionMethods/Startup/Swagger.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tokiota.BookStore.Web.ExtensionMethods.Startup
+{
+    public static class Swagger
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Library API", Version = "v1" });
+                //var filePath = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "Tokiota.Core.Web.XML");
+                //c.IncludeXmlComments(filePath);
+            });
+
+        }
+
+        public static void UseSwaggerAndUI(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUi(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Library API");
+            });
+
+        }
+    }
+}
